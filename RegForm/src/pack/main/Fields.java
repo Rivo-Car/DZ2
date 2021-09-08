83e7fe497c89129b16c8fa2f17bc3b8722fa268e
Rivo-Car <70138793+Rivo-Car@users.noreply.github.com>
2021-09-08 07:37:46 +0300

    Update Fields.java

--- RegForm/src/pack/main/Fields.java
+++ RegForm/src/pack/main/Fields.java
@@ -1,18 +1,28 @@
 package pack.main;
 
 public enum Fields {
-	First_name,
-	Surname,
-	Patronymic,
-	FIO,
-	Login,
-	Group,
-	Home_Phone,
-	Mobile_Phone,
-	Mobile_Phone_2,
-	Email,
-	Skype,
-	Address,
-	Date_of_Creation,
-	This_day
+	First_name("First name", "Iм'я"),
+	Surname("Surname", "Прiзвище"),	
+	Patronymic("Patronymic", "По-батьковi"),
+	FIO("Full name", "ПIБ"),
+	Login("Login", "Логiн"),
+	Group("Group", "Группа"),
+	Home_Phone("Home Phone", "Домашнiй телефон"),
+	Mobile_Phone("Mobile Phone", "Мобiльний телефон"),
+	Mobile_Phone_2("Mobile Phone 2", "Мобiльний телефон 2"),
+	Email("Email", "Електронна пошта"),
+	Skype("Skype", "Скайп"),
+	Address("Address", "Адреса"),
+	Date_of_Creation("Date of creation", "Дата створення списку"),
+	This_day("Today is", "Сьогоднi");	
+	private List<String> fields;
+	
+	Fields(String...args) {
+	    this.fields = List.of(args);
+	}
+	
+	public String value(int local) {
+		return fields.get(local);
+	}
 }
+
