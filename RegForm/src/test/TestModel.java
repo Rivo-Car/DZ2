45b89cac239f4bc697c39f332e3920185d73f9fe
Rivo-Car <eduard.billionaire@gmail.com>
2021-09-10 13:13:40 +0200

    Commit to class Test

--- RegForm/src/test/TestModel.java
+++ RegForm/src/test/TestModel.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import pack.main.Fields;
 import pack.main.Model;
+import pack.main.View;
 import pack.main2.*;
 
 /**
@@ -13,8 +15,12 @@ import pack.main2.*;
  *
  */
 class TestModel {
-    private Model2 M2 = new Model2(new Model("ASsssssss"), new Model("211313bhmgb"), new Model("Tycoon99"));
-	
+	Model M11 = new Model("ASsssssss");
+	Model M12 = new Model("211313bhmgb");
+	Model M13 = new Model("Tycoon99");
+    private Model2 M2 = new Model2(M11, M12, M13);
+    Fields field = Fields.Login;
+    
 	@Test
 	void TestIsAuthentic() {
 		Assertions.assertTrue(M2.isAuthentic("Casserta"), "");
@@ -25,6 +31,38 @@ class TestModel {
 		Assertions.assertTrue(M2.isAuthentic("ASssssSSss"), "");
 	}
 	
+	@Test
+	void TestMatches() {		
+		String pattern = View.bundleString("regex.login.data");
+		String[] ArrayString = Model.Groups;
+		String[] UrrayString = Model.Gruppy;
+		
+		Assertions.assertTrue(M11.matches("Passart12", pattern, field));
+		Assertions.assertTrue(M11.matches("Gorsows_12", pattern, field));
+		Assertions.assertTrue(M11.matches("Gorsows-12", pattern, field));
+		Assertions.assertTrue(M11.matches("12-Gorsows-12", pattern, field));
+		Assertions.assertFalse(M11.matches("Gorsows", pattern, field));
+		Assertions.assertFalse(M11.matches("Gоrsows3232", pattern, field));
+		
+		Assertions.assertTrue(M11.matches("FI-12", ArrayString));
+		Assertions.assertTrue(M11.matches("ФІ-13", UrrayString));
+		Assertions.assertFalse(M11.matches("ФI-13", UrrayString));
+		Assertions.assertFalse(M11.matches("FI-15", ArrayString));		
+	}
 	
+	@Test
+	void TestMatchesFields() {
+		Model M111 = new Model("ASsssssss");
+		Model M112 = new Model("ASssssss");
+		Model M121 = new Model("211313bhmgb");
+		Model M122 = new Model("215313bhmgb");
+		
+		Assertions.assertTrue(Model.matches("ASsssssss", M111, field));
+		Assertions.assertTrue(Model.matches("ASssssss", M112, field));
+		Assertions.assertFalse(Model.matches("ASssssss", M111, field));
+		Assertions.assertTrue(Model.matches("211313bhmgb", M121, field));
+		Assertions.assertTrue(Model.matches("215313bhmgb", M122, field));
+		Assertions.assertFalse(Model.matches("ASssssss", M121, field));		
+	}
 
 }
