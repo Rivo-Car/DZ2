3ef9a4cf0c35b24390124e842d392f6b99ed9cf0
Rivo-Car <70138793+Rivo-Car@users.noreply.github.com>
2021-09-10 23:48:58 +0300

    Add files via upload

---
+++ RegForm/src/pack/main/Main.java
@@ -0,0 +1,10 @@
+package pack.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Controller c = new Controller(new Model(), new View());
+     c.processUser();
+	}
+}
---
+++ RegForm/src/pack/main2/Main.java
@@ -0,0 +1,11 @@
+package pack.main2;
+
+public class Main {
+
+	public static void main(String[] args) throws UnicityException {
+		// TODO Auto-generated method stub
+       Controller2 C2 = new Controller2();
+       C2.processUser();
+	}
+
+}
