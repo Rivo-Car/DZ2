fda0bec5a23e889882b372f515e39a2bae9f870b
Rivo-Car <70138793+Rivo-Car@users.noreply.github.com>
2021-09-25 11:11:33 +0200

    Add files via upload

---
+++ Stream/src/main/IntStreamExample.java
@@ -0,0 +1,47 @@
+package main;
+import java.util.Arrays;
+import java.util.stream.*;
+
+public class IntStreamExample {
+
+	public static void main(String[] args) {
+		int multiplicator = 5;
+		int[] array = {};
+		double mean = Arrays.
+				stream(array).
+				average().
+				orElse(Double.NaN);		
+		int min = IntStream.
+				of(array).
+				sorted().
+				findFirst().
+				getAsInt();
+		int indexOfMin = IntStream
+				.range(0, array.length)
+                .filter(i -> min == array[i])
+                .findFirst()
+                .orElse(-1);
+		int zeroRepeat = IntStream
+				.range(0, array.length)
+				.filter(i -> 0 == array[i])
+				.toArray()
+				.length;	
+		int positives =  IntStream
+				.range(0, array.length)
+				.filter(i -> 0 < array[i])
+				.toArray()
+				.length;	
+		int[] moltipliedArray = IntStream
+				.of(array)
+				.map(i -> multiplicator * i)
+				.toArray();
+		System.err.println("Our array: " + Arrays.toString(array));
+		System.err.println("mean: " + mean);
+		System.err.println("min: " + min);
+		System.err.println("Index of minimum element: " + indexOfMin);
+		System.err.println("Number of 0 entrances: " + zeroRepeat);
+		System.err.println("Number of positive entrances: " + positives);
+		System.err.print("Multiplied by " + multiplicator + " array: ");
+		for(int i : moltipliedArray) System.err.print(i + " ");
+	}
+}
