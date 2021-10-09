eabfdbeccc6cff3443f0dfdf8f2bba46656f29af
Rivo-Car <70138793+Rivo-Car@users.noreply.github.com>
2021-10-09 21:50:25 +0300

    Add files via upload

---
+++ demo_jpa.zip_expanded/demo_jpa/src/main/java/ua/testing/demo_jpa/dto/UsersDTO.java
@@ -0,0 +1,19 @@
+package ua.testing.demo_jpa.dto;
+
+import lombok.*;
+import ua.testing.demo_jpa.entity.User;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class UsersDTO {
+    private List<User> users;
+    
+    public UsersDTO(List<User> users) {
+    	this.users = users;
+    }
+}
---
+++ demo_jpa.zip_expanded/demo_jpa/src/main/resources/public/mydb.sql
@@ -0,0 +1,14 @@
+create table NEUSERS
+(
+    id       INT,
+    firstName     VARCHAR(50),
+    lastName VARCHAR(50),
+    password VARCHAR(50),
+    email    VARCHAR(50)
+);
+
+insert into user (id, Firstname, Lastname, password, email) values
+ (1, 'Waldemar Drain','dfsff', 'KzPgkIhd', 'wdrain0@vistaprint.com');
+
+insert into user (id, Lastname, Lastname, password, email) values
+ (2, 'Shae Vickress', 'DFdf', 'Xhf8mvvBuCmC', 'svickress1@who.int');
